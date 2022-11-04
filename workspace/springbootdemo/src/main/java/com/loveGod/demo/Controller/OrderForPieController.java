edcb1f7692a39df8d31013eb916f085edc50878a
yingxiu0 <smile79653@gmail.com>
2022-11-04 11:23:25 +0800

    調整navbar Logo的位置，新增統計圖表

--- workspace/springbootdemo/src/main/java/com/loveGod/demo/Controller/OrderForPieController.java
+++ workspace/springbootdemo/src/main/java/com/loveGod/demo/Controller/OrderForPieController.java
@@ -7,20 +7,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.loveGod.demo.model.Order;
 import com.loveGod.demo.model.OrderDetail;
 import com.loveGod.demo.service.OrderDetailService;
+import com.loveGod.demo.service.OrderService;
 
 @Controller
 public class OrderForPieController {
 	
 	@Autowired
 	private OrderDetailService oDService;
+	@Autowired
+	private OrderService oService;
 	
 	
 	@GetMapping("/orderPie")
 	public String viewHomePage(Model model){
 		List<OrderDetail> orderDetail = oDService.listOrderDetail();
+		List<Order> order = oService.findAllOrder();
 		model.addAttribute("orderDetail", orderDetail);
+		model.addAttribute("order", order);
 	        return "/shop/orderPie";
 	 }
 

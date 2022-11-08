355288d236d0f36fa7f3d061fe3dd509db09422f
spadesAA <108362294+spadesAA@users.noreply.github.com>
2022-11-08 19:25:55 +0800

    更新無法使用驗證碼之問題

--- workspace/springbootdemo/src/main/java/com/loveGod/demo/SpringbootdemoApplication.java
+++ workspace/springbootdemo/src/main/java/com/loveGod/demo/SpringbootdemoApplication.java
@@ -2,6 +2,9 @@ package com.loveGod.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import captcha.CaptchaGenerator;
 
 @SpringBootApplication
 public class SpringbootdemoApplication {
@@ -10,4 +13,8 @@ public class SpringbootdemoApplication {
 		SpringApplication.run(SpringbootdemoApplication.class, args);
 	}
 
+	@Bean
+	public CaptchaGenerator getCaptchaGenerator() {
+		return new CaptchaGenerator();
+	}
 }
